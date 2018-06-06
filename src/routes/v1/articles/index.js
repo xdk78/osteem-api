bc3973b59ad62423b8c77efca02031f7b2cf7fca
xdk78 <xdk78@interia.pl>
2018-06-06 20:27:56 +0200

    Basic articles formating

--- src/routes/v1/articles/index.js
+++ src/routes/v1/articles/index.js
@@ -1,17 +1,19 @@
 const steem = require('steem')
+const removeMd = require('remove-markdown')
+
 module.exports = (fastify, opts, next) => {
   fastify.get('/articles/trending', opts, (req, reply) => {
     reply.header('Content-Type', 'application/json').code(200)
     const query = {
       tag: null,
       limit: 20,
-      truncateBody: 228,
+      truncate_body: 228,
       start_author: null,
       start_permlink: null
     }
-    steem.api.getDiscussionsByTrending(query, (err, result) => {
+    steem.api.getDiscussionsByTrending(query, (err, res) => {
       if (err) throw err
-      reply.send(result)
+      reply.send(formatResponse(res))
     })
   })
 
@@ -23,15 +25,44 @@ module.exports = (fastify, opts, next) => {
       const query = {
         tag: null,
         limit: 20,
-        truncateBody: 228,
+        truncate_body: 228,
         start_author: req.params.author,
         start_permlink: req.params.permlink
       }
-      steem.api.getDiscussionsByTrending(query, (err, result) => {
+      steem.api.getDiscussionsByTrending(query, (err, res) => {
         if (err) throw err
-        reply.send(result)
+
+        reply.send(formatResponse(res))
       })
     }
   })
   next()
+}
+const formatResponse = res => {
+  return res.map(el => {
+    const metadata = JSON.parse(el.json_metadata)
+    const tags = metadata.tags
+    const users = metadata.users
+    const images = metadata.image
+    const links = metadata.links
+    return {
+      id: el.id,
+      author: el.author,
+      permlink: el.permlink,
+      category: el.category,
+      parentAuthor: el.parent_author,
+      parentPermlink: el.parent_permlink,
+      title: el.title,
+      description: removeMd(el.body),
+      created: el.created,
+      depth: el.depth,
+      children: el.children,
+      tags: tags,
+      users: users,
+      images: images,
+      links: links,
+      pendingPayoutValue: el.pending_payout_value,
+      activeVotes: el.active_votes
+    }
+  })
 }
