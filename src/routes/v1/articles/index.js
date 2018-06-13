ec9e690d66d0bb6db15910edaa6c9f1ee5cfeeb0
xdk78 <xdk78@interia.pl>
2018-06-13 19:17:59 +0200

    Improve articles and comments

--- src/routes/v1/articles/index.js
+++ src/routes/v1/articles/index.js
@@ -13,56 +13,72 @@ module.exports = (fastify, opts, next) => {
     }
     steem.api.getDiscussionsByTrending(query, (err, res) => {
       if (err) throw err
-      reply.send(formatResponse(res))
+      reply.send(formatResponseArray(res))
     })
   })
 
-  fastify.get('/articles/trending/:author/:permlink', opts, (req, reply) => {
+  fastify.get('/articles/:author/:permlink', opts, (req, reply) => {
     reply.header('Content-Type', 'application/json').code(200)
     if (req.params === undefined || !req.params.author || !req.params.permlink) {
       reply.send({error: 'Missing query param'})
     } else {
-      const query = {
-        tag: null,
-        limit: 20,
-        truncate_body: 228,
-        start_author: req.params.author,
-        start_permlink: req.params.permlink
-      }
-      steem.api.getDiscussionsByTrending(query, (err, res) => {
+      steem.api.getContent(req.params.author, req.params.permlink, (err, res) => {
         if (err) throw err
-
-        reply.send(formatResponse(res))
+        reply.send(formatResponseObject(res))
       })
     }
   })
-  next()
-}
-const formatResponse = res => {
-  return res.map(el => {
-    const metadata = JSON.parse(el.json_metadata)
-    const tags = metadata.tags
-    const users = metadata.users
-    const images = metadata.image
-    const links = metadata.links
-    return {
-      id: el.id,
-      author: el.author,
-      permlink: el.permlink,
-      category: el.category,
-      parentAuthor: el.parent_author,
-      parentPermlink: el.parent_permlink,
-      title: el.title,
-      description: removeMd(el.body),
-      created: el.created,
-      depth: el.depth,
-      children: el.children,
-      tags: tags,
-      users: users,
-      images: images,
-      links: links,
-      pendingPayoutValue: el.pending_payout_value,
-      activeVotes: el.active_votes
+
+  fastify.get('/articles/:category/:author/:permlink/comments', opts, (req, reply) => {
+    reply.header('Content-Type', 'application/json').code(200)
+    if (req.params === undefined || !req.params.category | !req.params.author || !req.params.permlink) {
+      reply.send({error: 'Missing query param'})
+    } else {
+      steem.api.getState(`/${req.params.category}/@${req.params.author}/${req.params.permlink}`, (err, res) => {
+        if (err) throw err
+        reply.send(formatCommentsResponse(res))
+      })
     }
   })
+
+  next()
 }
+const formatCommentsResponse = res => {
+  const response = []
+  const content = res.content
+
+  Object.keys(content).forEach(key => {
+    response.push(content[key])
+  })
+  return response
+}
+
+const formatResponseObject = el => {
+  const metadata = JSON.parse(el.json_metadata)
+  const tags = metadata.tags
+  const users = metadata.users
+  const images = metadata.image
+  const links = metadata.links
+  return {
+    id: el.id,
+    author: el.author,
+    permlink: el.permlink,
+    category: el.category,
+    parentAuthor: el.parent_author,
+    parentPermlink: el.parent_permlink,
+    title: el.title,
+    description: removeMd(el.body),
+    replies: el.replies,
+    created: el.created,
+    depth: el.depth,
+    children: el.children,
+    tags: tags,
+    users: users,
+    images: images,
+    links: links,
+    pendingPayoutValue: el.pending_payout_value,
+    activeVotes: el.active_votes
+  }
+}
+
+const formatResponseArray = res => res.map(el => formatResponseObject(el))
